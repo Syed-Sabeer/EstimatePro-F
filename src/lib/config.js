93b839ab4449136587d5f353ba81738f4d5db36d
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2025-07-02 16:56:37 +0500

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,7 +1,7 @@
 // API Configuration
 export const API_CONFIG = {
   // BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api',
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://estimatepro.deveondynamics.com/public/api',
+  BASE_URL: import.meta.env.VITE_API_BASE_URL || 'https://huzaifa.softiv.co/api',
   TIMEOUT: 10000, // 10 seconds
 };
 
